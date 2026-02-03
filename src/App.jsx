54ccfe4608bf0028de2bcf28d884d1f359617cb6
Jeanette Wiktorsson <leiia@hotmail.com>
2026-02-03 09:22:05 +0100

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,29 @@ https://reactjs.org/docs/lists-and-keys.html
 /*Extra!
 Feel free to create more components, such as header/footer,
 or why not include some more data from the array? */
+import data from "./data.json";
 
 export const App = () => {
   return (
     <div className="App">
-      <p>Pokemon goes here</p>
+      <p>These are the Pokemons</p>
+      {data.pokemons.map((pokemon) => (
+        <div key={pokemon.id}>
+          <h3>{pokemon.name}</h3>
+          <h4>Types</h4>
+          <ul>{pokemon.types.map((type) => (
+            <li key={type}>{type}</li>
+          ))}
+          </ul>
+          <h4>Weaknesses</h4>
+          <ul>{pokemon.weaknesses.map((weaknesses) => (
+            <li key={weaknesses}>{weaknesses}</li>
+          ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
+
+
